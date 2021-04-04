fea105dca1edd9743162baad28113362512a10d7
bendrendel <bfdrendel@gmail.com>
2021-04-03 22:38:19 -0600

    Add local storage functionality

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
-const todoList = document.querySelector('.todo-list');
 const todoInput = document.querySelector('.todo-input');
+const todoContainer = document.querySelector('.todo-container');
+
+loadStorage();
 
 todoInput.addEventListener('keyup', e => {
     if (e.key === 'Enter' && e.target.value !== '') {
-        createTodo(e.target.value, todoList);
+        createTodo(e.target.value, false);
         e.target.value = '';
     }
-})
+});
 
-function createTodo(description, parentElement) {
+function createTodo(description, done) {
     const todoItem = document.createElement('p');
-    todoItem.classList.add('todo-item');
     todoItem.innerHTML = description;
+    todoItem.classList.add('todo-item');
+    
+    if (done) {
+        todoItem.classList.add('done');
+    }
+
     todoItem.addEventListener('click', e => {
-        if (e.button === 0) {
-            e.target.classList.toggle('done');
-        }
+        e.target.classList.toggle('done');
+        updateStorage();
     });
     todoItem.addEventListener('contextmenu', e => {
         e.preventDefault();
         e.target.remove();
+        updateStorage();
     })
     
-    parentElement.append(todoItem);
+    todoContainer.append(todoItem);
+    updateStorage();
+}
+
+function updateStorage() {
+    const todoItems = document.querySelectorAll('.todo-item');
+    const storedItems = [];
+
+    todoItems.forEach(todoItem => {
+        const storedItem = {
+            description: todoItem.innerHTML,
+            done: todoItem.classList.contains('done')
+        }
+        storedItems.push(storedItem);
+    });
+
+    localStorage.setItem('todos', JSON.stringify(storedItems));
+}
+
+function loadStorage() {
+    if (localStorage.getItem('todos')) {
+        const storedItems = JSON.parse(localStorage.getItem('todos'));
+        storedItems.forEach(storedItem => {
+            createTodo(storedItem.description, storedItem.done);
+        })
+    };
 }
